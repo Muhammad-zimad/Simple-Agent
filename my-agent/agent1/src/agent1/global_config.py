37f895be41db279f98b0962d1f02e84070f7ed39
Muhammad zimad <muhammadzimad156@gmail.com>
2025-05-08 14:26:31 +0500

    global_config.py

--- my-agent/agent1/src/agent1/global_config.py
+++ my-agent/agent1/src/agent1/global_config.py
@@ -1,7 +1,7 @@
-# import the required openai module to create Agent
-from agent1 import agent, Runner, AsyncOpenAi, set_default_openai_client, det_default_openai_api, set_tracking_disabled
+# import the required openai module to create Agent(Spell issue)
+from agents import Agent, Runner, AsyncOpenAI, set_default_openai_client, set_default_openai_api, set_tracing_disabled
 
-# import the dotenv and od module to run .env file
+# import the dotenv and os module to run .env file
 from dotenv  import load_dotenv
 import os
 
@@ -12,8 +12,8 @@ import asyncio
 load_dotenv()
 gemini_api_key =os.getenv("GEMINI_API_KEY")
 
-# setup the provider (3rd party)
-external_provider = AsyncOpenAi(
+# setup the provider (3rd party)(spell issue)
+external_provider = AsyncOpenAI(
     api_key = gemini_api_key,
     base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
 )
@@ -24,8 +24,8 @@ set_default_openai_client(external_provider)
 #setup api communication method
 set_default_openai_api("chat_completions")
 
-# Turning of the tracking
-set_tracking_disabled(True)
+# Turning of the tracking(wrong value)
+set_tracing_disabled(True)
 
 # setup Agent(with Instruction and model) and runnig with async function
 def run_global():
@@ -37,8 +37,8 @@ def run_global():
         )
         result = await Runner.run(
             agent,
-            "Say hello" ,
+            " hello how are you and what are you doing?" ,
         )
         # print the agent output
-        print(result)    
+        print(result.final_output)    
     asyncio.run(main())
